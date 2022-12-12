5066d741bdad369a40c2f073519d89ca4a80290a
michelepip <michele@michelepip.xyz>
2022-12-13 09:58:08 +1030

    [+] Modify TestModule.java

--- src/main/java/xyz/michelepip/cloudcommandframeworktestplugin/TestModule.java
+++ src/main/java/xyz/michelepip/cloudcommandframeworktestplugin/TestModule.java
@@ -1,18 +1,27 @@
 package xyz.michelepip.cloudcommandframeworktestplugin;
 
+import cloud.commandframework.annotations.Argument;
 import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
 import cloud.commandframework.annotations.CommandPermission;
-import cloud.commandframework.exceptions.CommandExecutionException;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class TestModule {
     @CommandPermission("cloudcommandframeworktestplugin.test")
     @CommandMethod("test")
     @CommandDescription("Test")
-    public void a(
-            CommandSender sender
+    public void TestCommand(
+            CommandSender sender,
+            @NotNull @Argument("some_string") String someString
     ) {
-        throw new CommandExecutionException(new Throwable("a"));
+        Player p = (Player) sender;
+        p.sendMessage(Component.text()
+                .append(Component.text("You typed: ", NamedTextColor.GOLD))
+                .append(Component.text(someString, NamedTextColor.LIGHT_PURPLE))
+                .build());
     }
 }
